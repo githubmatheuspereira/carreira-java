d25374448d8a09575dcc154ff21cc4db2648a09b
Matheus Pereira <contatomatheuspereirasilva@gmail.com>
2026-06-27 18:44:39 -0300

    adicionado getters e setters na classe musica

--- src/main/java/com/carreira/java/modulo02/desafios/Musica.java
+++ src/main/java/com/carreira/java/modulo02/desafios/Musica.java
@@ -2,31 +2,79 @@ package com.carreira.java.modulo02.desafios;
 
 public class Musica {
 
-    String nomeMusica;
-    String nomeArtista;
-    int anoLancamento;
-    double duracao;
-    String participacao;
-    String produtorMusical;
+    private String nomeMusica;
+    private String nomeArtista;
+    private int anoLancamento;
+    private double duracao;
+    private String participacao;
+    private String produtorMusical;
 
-    void exibirInformacao() {
-        System.out.printf("\nNome da música: %s\nNome do Artista: %s\nLançamento: %d\nDuração: %.2f\n%s%s",nomeMusica, nomeArtista, anoLancamento, duracao, adicionarParticipacao(participacao), adicionarProdutorMusical(produtorMusical));
+    public String getNomeMusica() {
+        return nomeMusica;
     }
 
-    String adicionarParticipacao(String participacao) {
+    public void setNomeMusica(String nomeMusica) {
+        this.nomeMusica = nomeMusica;
+    }
+
+    public String getNomeArtista() {
+        return nomeArtista;
+    }
+
+    public void setNomeArtista(String nomeArtista) {
+        this.nomeArtista = nomeArtista;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getParticipacao() {
+        return participacao;
+    }
+
+    public void setParticipacao(String participacao) {
+        this.participacao = participacao;
+    }
+
+    public String getProdutorMusical() {
+        return produtorMusical;
+    }
+
+    public void setProdutorMusical(String produtorMusical) {
+        this.produtorMusical = produtorMusical;
+    }
+
+    public void exibirInformacao() {
+        System.out.printf("\nNome da música: %s\nNome do Artista: %s\nLançamento: %d\nDuração: %.2f\n%s%s",getNomeMusica(), getNomeArtista(), getAnoLancamento(), getDuracao(), adicionarParticipacao(participacao), adicionarProdutorMusical(produtorMusical));
+    }
+
+    private String adicionarParticipacao(String participacao) {
         if (participacao != null) {
-            String part = participacao;
             return "Participação: " + participacao + "\n";
         }
         return "";
     }
 
-    String adicionarProdutorMusical(String produtorMusical) {
+    private String adicionarProdutorMusical(String produtorMusical) {
         if (produtorMusical != null) {
-            String produtor = produtorMusical;
             return "Produtor/DJ: " + produtorMusical + "\n";
         }
         return "";
     }
+
+
 
 }
